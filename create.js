135c3ddd6bc384fd38eaae17931e77c9398ebb13
Kams2024 <kamini.malotia@gmail.com>
2025-05-13 15:24:55 +0200

    Update create.js

--- create.js
+++ create.js
@@ -1,31 +1,34 @@
 function savecard() {
-const q=document.getElementById("question").value.trim();
-const a=document.getElementById("answer").value.trim();
-
-    if (q && a){
-        let cards=JSON.parse(localStorage.getItem("flashcards")) || [];
-        cards.push({question:q,answer:a})
-        localStorage.setItem("flashcards",JSON.stringify(cards))
-        document.getElementById("status").textContent = "Flashcard saved";
-        document.getElementById("question").value="";
-        document.getElementById("answer").value="";
-    }else{
-        document.getElementById("status").textContent="Please fill both fields"
+    const question=document.getElementById("question").value.trim();
+    const answer=document.getElementById("answer").value.trim();
+    const difficulty = document.getElementById("difficulty").value;
+    const category = document.getElementById("category").value.trim();
+    
+        if (question && answer){
+            let cards=JSON.parse(localStorage.getItem("flashcards")) || [];
+            cards.push({question,answer,difficulty,category});
+            localStorage.setItem("flashcards",JSON.stringify(cards));
+            document.getElementById("status").textContent = "Flashcard saved";
+            document.getElementById("question").value="";
+            document.getElementById("answer").value="";
+            document.getElementById("category").value = "";
+        }else{
+            document.getElementById("status").textContent="Please fill both fields"
+        }
+    
     }
-
-}
-function downloadcards() {
-const cards=JSON.parse(localStorage.getItem("flashcards"));
-const blob=new Blob([JSON.stringify (cards)],{type:"application/json"});
-const url=URL.createObjectURL(blob);
-
-
-const link =document.createElement("a");
-link.href=url;
-link.download="flashcards.json";
-link.click();
-URL.revokeObjectURL(url);
-setTimeout(() =>{
-    document.getElementById("status").textContent = "File Downloaded successfully";
-},1000);
-}
+    function downloadcards() {
+    const cards=JSON.parse(localStorage.getItem("flashcards"));
+    const blob=new Blob([JSON.stringify (cards)],{type:"application/json"});
+    const url=URL.createObjectURL(blob);
+    
+    
+    const link =document.createElement("a");
+    link.href=url;
+    link.download="flashcards.json";
+    link.click();
+    URL.revokeObjectURL(url);
+    setTimeout(() =>{
+        document.getElementById("status").textContent = "File Downloaded successfully";
+    },1000);
+    }
